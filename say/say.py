2b17691f739698edb7b9ce695b3d866d5b1033a4
highlizard <wizard8lizard@yahoo.com>
2016-11-13 14:25:40 +0900

    say it again

---
+++ say/say.py
@@ -0,0 +1,35 @@
+from cogs.utils import checks
+from discord.ext import commands
+
+
+class Say:
+    """I'll repeat what you said."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def say(self, *text):
+        """I'll repeat what you said."""
+
+        text = " ".join(text)
+        await self.bot.say(text)
+
+    @commands.command()
+    async def sonar(self, channelid, *, text):
+        """I'll repeat what you said and where you want it.
+        A modified version of the debug command, with help from Calebj."""
+
+        text = text.replace("\'", "\\\'")
+        channelid = self.bot.get_channel(str(channelid))
+        return await self.bot.send_message(channelid, text)
+
+    @commands.command(pass_context=True)
+    async def chinfo(self, ctx):
+        """Gets current channel ID immediately."""
+        channel = ctx.message.channel
+        return await self.bot.say(channel.id)
+
+
+def setup(bot):
+    bot.add_cog(Say(bot))
