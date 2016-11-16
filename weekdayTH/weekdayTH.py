0bb7eab058a0778393a902bf24a2fea7237fb362
highlizard <wizard8lizard@yahoo.com>
2016-11-16 16:22:38 +0900

    Update weekdayTH.py

--- weekdayTH/weekdayTH.py
+++ weekdayTH/weekdayTH.py
@@ -12,43 +12,43 @@ class WeekdayTH:
     async def sun(self):
         """Hello Monday!"""
 
-        await self.bot.say("สวัสดีวันอาทิตย์ ขอให้จิตสดใส")
+        await self.bot.say(":rose: สวัสดีวันอาทิตย์ ขอให้จิตสดใส! :rose:")
         
     @commands.command()
     async def mon(self):
         """Hello Monday!"""
 
-        await self.bot.say("สวัสดีวันจันทร์ ต้อนรับวันใหม่")
+        await self.bot.say(":hatched_chick: สวัสดีวันจันทร์ ต้อนรับวันใหม่ :hatched_chick:")
 
     @commands.command()
     async def tue(self):
         """This does stuff!"""
 
-        await self.bot.say("Hello Tuesday!")
+        await self.bot.say(":cherry_blossom: สวัสดีวันอังคาร ใจใสเบิกบาน! :cherry_blossom:")
 
     @commands.command()
     async def wed(self):
         """This does stuff!"""
 
-        await self.bot.say("Hello Wednesday!")
+        await self.bot.say(":four_leaf_clover: สวัสดีวันพุธ ดีสุดสุดวันพุธสดใส! :four_leaf_clover:")
 
     @commands.command()
     async def thu(self):
         """This does stuff!"""
 
-        await self.bot.say("Hello Thursday!")
+        await self.bot.say(":tangerine: สวัสดีวันพฤหัสบดี ขอให้ทุกท่านมีความสุข! :tangerine: ")
 
     @commands.command()
     async def fri(self):
         """This does stuff!"""
 
-        await self.bot.say("Hello Friday!")
+        await self.bot.say(":butterfly: สวัสดีวันศุกร์ ขอให้มีความสุขกันทั้งวัน! :butterfly:")
 
     @commands.command()
     async def sat(self):
         """This does stuff!"""
 
-        await self.bot.say("Hello Saturday!")
+        await self.bot.say(":grapes: สวัสดีวันเสาร์ ไม่เหงาไม่เศร้า! :grapes:")
 
 
 def setup(bot):
