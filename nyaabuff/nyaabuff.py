ce02d5f388f1cb67289b200cabd926288649d34b
highlizard <wizard8lizard@yahoo.com>
2018-02-27 18:34:21 +0900

    Update nyaabuff.py

--- nyaabuff/nyaabuff.py
+++ nyaabuff/nyaabuff.py
@@ -1,84 +1,32 @@
 import discord
 from discord.ext import commands
-from random import choice as rndchoice
 from .utils.dataIO import fileIO
+from random import choice as randchoice
 import os
-
-defaults = [
-    "HP",
-    "MP",
-    "Physical Damage",
-    "Magical Damage",
-    "Physical Critical Damage",
-    "Magical Critical Damage",
-    "Physical Critical Rate",
-    "Magical Critical Rate",
-    "Physical Defense Penetration",
-    "Magical Defense Penetration",
-    "Physical Defense",
-    "Magical Defense",
-    "Skill Cooldown Reduction",
-    "Skill Cost Reduction",
-    "Attack Speed",
-    "Movement Speed",
-    "Phase Power Charge Rate",
-    "Phase Power Release Duration",
-    "Phase Power Release Damage Boost",
-    "Additional Damage during Aerial Attacks",
-    "Critical Rate during Aerial Attacks",
-    "Defense Penetration Rate during Aerial Attacks",
-    "Critical Damage during Aerial Attacks",
-    "Additional Damage during Back Attacks",
-    "Critical Rate during Back Attacks",
-    "Defense Penetration Rate during Back Attacks",
-    "Critical Damage during Back Attacks",
-    "Additional Damage during Chase Attacks",
-    "Critical Rate during Chase Attacks",
-    "Defense Penetration Rate during Chase Attacks",
-    "Critical Damage during Chase Attacks",
-    "Additional Credits",
-    "Additional Item Drop Rate",
-    "Additional Experience",
-    "HP Regenerated per 3 Seconds",
-    "MP Regenerated per 3 Seconds",]
-    
 
 
 class Nyaabuff:
-    """Nyaa buffing command."""
 
+    """Airenkun's Insult Cog"""
     def __init__(self, bot):
         self.bot = bot
-        self.items = fileIO("data/nyaabuff/items.json", "load")
+        self.insults = fileIO("data/nyaabuff/nyaabuff.json","load")
 
-    @commands.command()
+    @commands.command(pass_context=True, no_pm=True)
     async def nyaabuff(self, ctx, user : discord.Member=None):
         """Give A Nyaa buff to users"""
+
         msg = ' '
         if user != None:
             if user.id == self.bot.user.id:
                 user = ctx.message.author
-                msg = " How original. No one else had thought of trying to get the bot to insult itself. I applaud your creativity. Yawn. Perhaps this is why you don't have friends. You don't add anything new to any conversation. You are more of a bot than me, predictable answers, and absolutely dull to have an actual conversation with."
-                await self.bot.say(user.mention + msg)
+                msg = "feels your Nyaa"
+                await self.bot.say(user.name + msg)
             else:
-                await self.bot.say(user.mention + msg + randchoice(self.items))
+                await self.bot.say(user.name + msg + randchoice(self.nyaabuff))
         else:
-            await self.bot.say(ctx.message.author.mention + msg + randchoice(self.items))
-
-def check_folders():
-    if not os.path.exists("data/nyaabuff"):
-        print("Creating data/nyaabuff folder...")
-        os.makedirs("data/nyaabuff")
-
-def check_files():
-    f = "data/nyaabuff/items.json"
-    if not fileIO(f, "check"):
-        print("Creating empty items.json...")
-        fileIO(f, "save", defaults)
+            await self.bot.say(ctx.message.author.mention + msg + randchoice(self.nyaabuff))
 
 
 def setup(bot):
-    check_folders()
-    check_files()
-    n = Nyaabuff(bot)
-    bot.add_cog(n)
+    bot.add_cog(Nyaabuff(bot))
